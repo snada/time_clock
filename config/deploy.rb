9b78800eae093e8a5823b1473158f874d7e0b350
snada <stefano.nada@gmail.com>
2019-11-05 02:31:09 -0500

    [5] Overriding yarn install

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,12 @@ set :keep_releases, 3
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+namespace :yarn do
+  task :install do
+    execute :yarn, 'install'
+  end
+end
+
 namespace :deploy do
   before :compile_assets, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
